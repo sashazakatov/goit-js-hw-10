4e6ba3dca8014e59ae73ba2c7167312f8eaa1969
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-03-06 00:04:45 +0200

    adds first task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import {fetchCountries} from './js/fetchCountries'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    searchBox: document.querySelector('#search-box'),
+}
+
+refs.searchBox.addEventListener('input', debounce(OnSearchBoxClick, DEBOUNCE_DELAY));
+
+function OnSearchBoxClick(e){
+    fetchCountries(e.target.value)
+    .then(Response => {
+        if(Response.length >= 10){
+            Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+        console.log(Response);
+    })
+    .catch((error)=>{
+        Notify.failure(error);
+    });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+export const fetchCountries = (name = '') => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(Response => {
+        if(!Response.ok){
+            return Promise.reject('Oops, there is no country with that name');
+        }    
+        return Response.json()
+    });
+}
