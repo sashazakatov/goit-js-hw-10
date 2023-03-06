ea825e138ee4ce221ffcd497162b9abcf72c13a3
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-03-06 13:43:38 +0200

    updates the task

--- src/index.js
+++ src/index.js
@@ -17,53 +17,59 @@ refs.searchBox.addEventListener('input', debounce(OnSearchBoxClick, DEBOUNCE_DEL
 
 function OnSearchBoxClick(e){
     const value = e.target.value.trim();
-    console.log(value);
-    if(value === ''){
-        refs.countryInfo.innerHTML = '';
-        refs.countryList.innerHTML = '';
-        return;
-    }
+
+    // fff(value);
+
     fetchCountries(value)
     .then(Response => {
         if(Response.length >= 10){
             Notify.info('Too many matches found. Please enter a more specific name.');
-            return;
-        }
-        if(Response.length >= 2 && Response.length <= 10){
-            refs.countryInfo.innerHTML = '';
-            const markup = Response.map(({flags:{svg, alt}, name:{official}})=>{
-                return `
-                <li>
-                    <img width="20" height="14" src="${svg}" alt="${alt}">
-                    <span>${official}</span>
-                </li>`;
-            }).join('');
-            refs.countryList.innerHTML = markup;
-            return;
         }
         if(Response.length === 1){
             refs.countryList.innerHTML = '';
-            const markup = Response.map(({flags:{svg, alt}, name:{official}, capital, population, languages})=>{
-                return `
-                    <img width="20" height="14" src="${svg}" alt="${alt}">
-                    <span>${official}</span>
-                    <ul>
-                        <li>
-                            <span>Capitl:</span> ${capital}
-                        </li>
-                        <li>
-                            <span>Population:</span> ${population}
-                        </li>
-                        <li>
-                            <span>Languages:</span> ${languages}
-                        </li>
-                    </ul>`;
-            })
-            refs.countryInfo.innerHTML = markup;
-            return;
+            refs.countryInfo.innerHTML = createCountryInfoMarkup();
+        }
+        if(Response.length >= 2 && Response.length <= 10){
+            refs.countryInfo.innerHTML = '';
+            refs.countryList.innerHTML = createCountryListMarkup();
         }
     })
     .catch((error)=>{
         Notify.failure(error);
     });
+}
+
+function fff(value = ''){
+    if(value === ''){
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+    }
+}
+function createCountryInfoMarkup(){
+    return Response.map(({flags:{svg, alt}, name:{official}, capital, population, languages})=>{
+        return `
+            <img width="20" height="14" src="${svg}" alt="${alt}">
+            <span>${official}</span>
+            <ul>
+                <li>
+                    <span>Capitl:</span> ${capital}
+                </li>
+                <li>
+                    <span>Population:</span> ${population}
+                </li>
+                <li>
+                    <span>Languages:</span> ${languages}
+                </li>
+            </ul>`;
+    })
+}
+function getCountryListMarkup(){
+    return Response.map(({flags:{svg, alt}, name:{official}})=>{
+        return `
+        <li>
+            <img width="20" height="14" src="${svg}" alt="${alt}">
+            <span>${official}</span>
+        </li>`;
+    }).join('');
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,11 @@
+const URL = 'https://restcountries.com';
 const searchParams = new URLSearchParams({
     fields: 'name,capital,population,flags,languages'
 });
 
 
 export const fetchCountries = (name = '') => {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    return fetch(`${URL}/v3.1/name/${name}?${searchParams}`)
     .then(Response => {
         if(!Response.ok){
             return Promise.reject('Oops, there is no country with that name');
